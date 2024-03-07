1fbac1ab69f8e2731dbb4c91f67ae23be5014997
Clint-Mathews <mathewsclint28@gmail.com>
2024-03-07 10:24:13 +0530

    Added hello_world using rustc and hello_cargo using cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795.d
@@ -0,0 +1,5 @@
+/Users/clint/Learning/Learn-Rust/hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795: src/main.rs
+
+/Users/clint/Learning/Learn-Rust/hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-e2b4037491bffce6.d
@@ -0,0 +1,5 @@
+/Users/clint/Learning/Learn-Rust/hello_cargo/target/debug/deps/libhello_cargo-e2b4037491bffce6.rmeta: src/main.rs
+
+/Users/clint/Learning/Learn-Rust/hello_cargo/target/debug/deps/hello_cargo-e2b4037491bffce6.d: src/main.rs
+
+src/main.rs:
